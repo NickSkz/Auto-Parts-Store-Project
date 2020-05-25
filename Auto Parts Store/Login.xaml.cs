c4ce5ca47a2118ce26a89ffc71a91c52ed1ebb0a
NickSkz <skubiszmikolaj450@gmail.com>
2020-05-25 03:01:06 +0200

    Added WPF .NET Core App.

---
+++ Auto Parts Store/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Auto_Parts_Store
+{
+    /// <summary>
+    /// Interaction logic for LogSign.xaml
+    /// </summary>
+    public partial class LogSign : Window
+    {
+        public LogSign()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
